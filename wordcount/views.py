9e333f059a267f6bd23c45d46849884efc01dab3
Minsu <plugnplay1007@gmail.com>
2019-03-05 17:06:24 +0900

    bootstrap_version

--- wordcount/views.py
+++ wordcount/views.py
@@ -9,11 +9,18 @@ def about(request):
 
 def result(request):
     text=request.GET["fulltext"]
+    var_for_count=list(text)
+    sum=0
+    length=len(var_for_count)
+    for i in var_for_count:
+        if i!=" " and i!="\n":
+            sum+=1
     words=text.split()
     word_dictionary={}
+    
     for word in words:
         if word in word_dictionary:
             word_dictionary[word]+=1
         else:
             word_dictionary[word]=1
-    return render(request,"result.html",{'full':text,"total":len(words),"dictionary":word_dictionary.items()})       
+    return render(request,"result.html",{'sum':sum,'length':length,'full':text,"total":len(words),"dictionary":word_dictionary.items()})       
